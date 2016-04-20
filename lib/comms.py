987b30935ffae2dffe801f5328888cf14e6e2629
Andrew <andrew.pope456@gmail.com>
2016-04-20 03:06:46 -0400

    Comms: - Set Verbose mode to TRUE to gather more information in bot information transfer

--- lib/comms.py
+++ lib/comms.py
@@ -10,7 +10,8 @@ class StealthConn(object):
         self.cipher = None
         self.client = client
         self.server = server
-        self.verbose = verbose
+        # self.verbose = verbose - TODO: This and the below line of code is used for verbose output, must remove
+        self.verbose = True
         self.initiate_session()
 
     def initiate_session(self):
@@ -20,18 +21,25 @@ class StealthConn(object):
         # This can be broken into code run just on the server or just on the client
         if self.server or self.client:
             my_public_key, my_private_key = create_dh_key()
+            
             # Send them our public key
             self.send(bytes(str(my_public_key), "ascii"))
-            # Receive their public key
+            
+            # Receive their public key - Used in Cipher as encryption key
             their_public_key = int(self.recv())
+            
             # Obtain our shared secret
             shared_hash = calculate_dh_secret(their_public_key, my_private_key)
             print("Shared hash: {}".format(shared_hash))
 
         # Default XOR algorithm can only take a key of length 32
-        self.cipher = XOR.new(shared_hash[:4])
+        self.cipher = XOR.new(shared_hash[:4]) #TODO: Replace with AES cipher method
+        
+        # cipher = AES.new(their_public_key,         
 
     def send(self, data):
+        #TODO: Add Anti-Replay Mechanism and HMAC        
+
         if self.cipher:
             encrypted_data = self.cipher.encrypt(data)
             if self.verbose:
@@ -39,14 +47,17 @@ class StealthConn(object):
                 print("Encrypted data: {}".format(repr(encrypted_data)))
                 print("Sending packet of length {}".format(len(encrypted_data)))
         else:
+            # If the cipher has not been created, just send the plaintext data (BAD)
             encrypted_data = data
 
         # Encode the data's length into an unsigned two byte int ('H')
         pkt_len = struct.pack('H', len(encrypted_data))
         self.conn.sendall(pkt_len)
         self.conn.sendall(encrypted_data)
+        #TODO: Remember to send HMAC too AND IV
 
     def recv(self):
+        #TODO: Add Anti-Replay Mechanism and HMAC
         # Decode the data's length from an unsigned two byte int ('H')
         pkt_len_packed = self.conn.recv(struct.calcsize('H'))
         unpacked_contents = struct.unpack('H', pkt_len_packed)
@@ -64,13 +75,15 @@ class StealthConn(object):
 
         return data
     
-    def encrypt(self):
+    def encrypt(self, data):
         print("Encrypting...") #Remove when function implemented
         ## TODO - Implement block cipher
+        # cipher = AES.new(their_public_key, MODE, iv)
     
-    def decrypt(self):
+    def decrypt(self, data):
         print("Decrypting...") #Remove when function implemented
         ## TODO - Implement block cipher
+        # cipher = AES.new(our_private_key, MODE, iv)
         
 
     def close(self):
