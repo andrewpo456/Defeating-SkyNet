633c97445c201195519cbb4a7ed33572aff5cbf2
Andrew <andrew.pope456@gmail.com>
2016-04-22 02:15:55 -0400

    Comms: - Swapped tabs to spaces - Created the two private methods packet_send/recv. These methods are used to send certain data packet strucures through the 'network' (through conn)

--- lib/comms.py
+++ lib/comms.py
@@ -1,85 +1,134 @@
 import struct
-
+from Crypto.Hash import SHA256
+from Crypto.Random import random
 from Crypto.Cipher import XOR
-
 from dh import create_dh_key, calculate_dh_secret
 
 class StealthConn(object):
-    def __init__(self, conn, client=False, server=False, verbose=False):
-        self.conn = conn
-        self.encryptCipher = None
-        self.decryptCipher = None
-        self.client = client
-        self.server = server
-        # self.verbose = verbose - TODO: This and the below line of code is used for verbose output, must remove
-        self.verbose = True
-        self.initiate_session()
-    
-    def __print_verbose(self, str):
-        if self.verbose:
-          print(str)    
-    
-    def initiate_session(self):
-        # Perform the initial connection handshake for agreeing on a shared secret
-        # TODO: Is their initial connection work here?
+  def __init__(self, conn, client=False, server=False, verbose=True):
+    self.conn = conn
+    self.encryptCipher = None
+    self.decryptCipher = None
+    self.client = client
+    self.server = server
+    self.verbose = verbose
+    self.initiate_session()
 
-        # This can be broken into code run just on the server or just on the client
-        if self.server or self.client:
-            my_public_key, my_private_key = create_dh_key()
-            
-            # Send them our public key
-            self.send(bytes(str(my_public_key), "ascii"))
-            
-            # Receive their public key - Used in Cipher as encryption key
-            their_public_key = int(self.recv())
-            
-            # Obtain our shared secret
-            shared_hash = calculate_dh_secret(their_public_key, my_private_key)
-            print("Shared hash: {}".format(shared_hash))
-
-        # Default XOR algorithm can only take a key of length 32 - TODO: Implement AES cipher
-        self.encryptCipher = XOR.new(shared_hash[:4]) # cipher = AES.new(their_public_key, MODE, iv)
-        self.decryptCipher = XOR.new(shared_hash[:4]) # cipher = AES.new(our_private_key, MODE, iv)
-
-    def send(self, data):
-        #TODO: Add Anti-Replay Mechanism and HMAC        
-
-        if self.encryptCipher:
-            encrypted_data = self.encryptCipher.encrypt(data)
+  def __print_verbose(self, string):
+    if self.verbose:
+      print(string)
    
-            self.__print_verbose("Original data: {}".format(data))
-            self.__print_verbose("Encrypted data: {}".format(repr(encrypted_data)))
-            self.__print_verbose("Sending packet of length {}".format(len(encrypted_data)))
-        else:
-            # If the cipher has not been created, just send the plaintext data (BAD)
-            encrypted_data = data
+  def __packet_send(self, data):
+    """ 
+    Sends data in a 'packet'
+    Send 1: Contains the length of data
+    Send 2: Contains the data itself
+    """
+    # Encode the data's length into an unsigned two byte int ('H')
+    pkt_len = struct.pack('H', len(data))
+    self.conn.sendall(pkt_len)
+    self.conn.sendall(data)
 
-        # Encode the data's length into an unsigned two byte int ('H')
-        pkt_len = struct.pack('H', len(encrypted_data))
-        self.conn.sendall(pkt_len)
-        self.conn.sendall(encrypted_data)
-        #TODO: Remember to send HMAC too AND IV
+  def __packet_recv(self):
+    """ 
+    Recieves a packet from the network
+    and returns data + length of packet
+    """
+    pkt_len_packed = self.conn.recv(struct.calcsize('H'))
+    pkt_len = struct.unpack('H', pkt_len_packed)[0]
+    
+    return self.conn.recv(pkt_len), pkt_len
 
-    def recv(self):
-        #TODO: Add Anti-Replay Mechanism and HMAC
+  def __sync_session_nonce(self, shared_hash):
+    """
+    Used to generate and synchronise nonces between
+    client and server. This is to prevent relplay
+    attacks.
+    """
+    if self.server:
+      # Generate the pseudorandom server nonce & send to client
+      snonce = bytes(str(random.randint(1, 2**4096)), "ascii")
+      self.__packet_send(snonce)
+      
+      # Recieve the session_hash of client and server nonces from the client
+      session_hash = self.__packet_recv()
+      
+    if self.client:
+      # Generate the pseudorandom client nonce and recieve the server nonce
+      cnonce = bytes(str(random.randint(1, 2**4096)), "ascii")
+      snonce = self.__packet_recv()
 
-        # Decode the data's length from an unsigned two byte int ('H')        
-        pkt_len_packed = self.conn.recv(struct.calcsize('H'))
-        pkt_len = struct.unpack('H', pkt_len_packed)[0]
-        
-        # Recieve the encrypted data
-        encrypted_data = self.conn.recv(pkt_len)
+      # Calculate the session nonces with shared secret hash
+      session_nonce = snonce + cnonce + shared_hash
+      session_hash = SHA256.new(bytes(session_nonce)).hexdigest()
+      
+      # Encode the data's length into an unsigned two byte int ('H')
+      self.__packet_send(session_hash)
+    
+  def initiate_session(self):
+    """
+    Initiates session between client and server bots
+    """
+    # Perform the initial connection handshake for agreeing on a shared secret
+    # TODO: Is there initial connection work here?
 
-        if self.decryptCipher:
-            data = self.decryptCipher.decrypt(encrypted_data)
-            
-            self.__print_verbose("Receiving packet of length {}".format(pkt_len))
-            self.__print_verbose("Encrypted data: {}".format(repr(encrypted_data)))
-            self.__print_verbose("Original data: {}".format(data))    
-        else:
-            data = encrypted_data
+    # This can be broken into code run just on the server or just on the client
+    if self.server or self.client:
+      my_public_key, my_private_key = create_dh_key()
 
-        return data
+      # Send them our public key
+      self.send(bytes(str(my_public_key), "ascii"))
 
-    def close(self):
-        self.conn.close()
+      # Receive their public key - Used in Cipher as encryption key
+      their_public_key = int(self.recv())
+
+      # Obtain our shared secret
+      shared_hash = calculate_dh_secret(their_public_key, my_private_key)
+      print("Shared hash: {}".format(shared_hash))
+       
+      # self.__sync_session_nonce(shared_hash)
+
+
+    # Default XOR algorithm can only take a key of length 32 - TODO: Implement AES cipher
+    self.encryptCipher = XOR.new(shared_hash[:4]) # cipher = AES.new(their_public_key, MODE, iv)
+    self.decryptCipher = XOR.new(shared_hash[:4]) # cipher = AES.new(our_private_key, MODE, iv)
+
+  def send(self, data):
+    """
+    Encrypt and send data over the network.
+    """
+    #TODO: Add Anti-Replay Mechanism
+    if self.encryptCipher:
+      encrypted_data = self.encryptCipher.encrypt(data)
+
+      self.__print_verbose("Original data: {}".format(data))
+      self.__print_verbose("Encrypted data: {}".format(repr(encrypted_data)))
+      self.__print_verbose("Sending packet of length {}".format(len(encrypted_data)))
+    else:
+      # If the cipher has not been created, just send the plaintext data (BAD)
+      encrypted_data = data
+
+    #TODO: Remember to send HMAC too AND IV
+    self.__packet_send(encrypted_data)
+
+  def recv(self):
+    """
+    Recieve and decrypt data from the network.
+    """
+    #TODO: Add Anti-Replay Mechanism and HMAC
+    # Recieve the encrypted data        
+    encrypted_data, pkt_len = self.__packet_recv()
+
+    if self.decryptCipher:
+      data = self.decryptCipher.decrypt(encrypted_data)
+
+      self.__print_verbose("Receiving packet of length {}".format(pkt_len))
+      self.__print_verbose("Encrypted data: {}".format(repr(encrypted_data)))
+      self.__print_verbose("Original data: {}".format(data))    
+    else:
+      data = encrypted_data
+
+    return data
+
+  def close(self):
+    self.conn.close()
