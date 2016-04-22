767aadb3148405d86d446635366263b8e64198bc
Andrew <andrew.pope456@gmail.com>
2016-04-22 00:32:11 -0400

    Comms: - Created the private print_verbose method which prints verbose messages - Created the two class variables encrypt/decrypt Cipher

--- lib/comms.py
+++ lib/comms.py
@@ -7,17 +7,22 @@ from dh import create_dh_key, calculate_dh_secret
 class StealthConn(object):
     def __init__(self, conn, client=False, server=False, verbose=False):
         self.conn = conn
-        self.cipher = None
+        self.encryptCipher = None
+        self.decryptCipher = None
         self.client = client
         self.server = server
         # self.verbose = verbose - TODO: This and the below line of code is used for verbose output, must remove
         self.verbose = True
         self.initiate_session()
-
+    
+    def __print_verbose(self, str):
+        if self.verbose:
+          print(str)    
+    
     def initiate_session(self):
         # Perform the initial connection handshake for agreeing on a shared secret
+        # TODO: Is their initial connection work here?
 
-        ### TODO: Your code here!
         # This can be broken into code run just on the server or just on the client
         if self.server or self.client:
             my_public_key, my_private_key = create_dh_key()
@@ -32,20 +37,19 @@ class StealthConn(object):
             shared_hash = calculate_dh_secret(their_public_key, my_private_key)
             print("Shared hash: {}".format(shared_hash))
 
-        # Default XOR algorithm can only take a key of length 32
-        self.cipher = XOR.new(shared_hash[:4]) #TODO: Replace with AES cipher method
-        
-        # cipher = AES.new(their_public_key,         
+        # Default XOR algorithm can only take a key of length 32 - TODO: Implement AES cipher
+        self.encryptCipher = XOR.new(shared_hash[:4]) # cipher = AES.new(their_public_key, MODE, iv)
+        self.decryptCipher = XOR.new(shared_hash[:4]) # cipher = AES.new(our_private_key, MODE, iv)
 
     def send(self, data):
         #TODO: Add Anti-Replay Mechanism and HMAC        
 
-        if self.cipher:
-            encrypted_data = self.cipher.encrypt(data)
-            if self.verbose:
-                print("Original data: {}".format(data))
-                print("Encrypted data: {}".format(repr(encrypted_data)))
-                print("Sending packet of length {}".format(len(encrypted_data)))
+        if self.encryptCipher:
+            encrypted_data = self.encryptCipher.encrypt(data)
+   
+            self.__print_verbose("Original data: {}".format(data))
+            self.__print_verbose("Encrypted data: {}".format(repr(encrypted_data)))
+            self.__print_verbose("Sending packet of length {}".format(len(encrypted_data)))
         else:
             # If the cipher has not been created, just send the plaintext data (BAD)
             encrypted_data = data
@@ -58,33 +62,24 @@ class StealthConn(object):
 
     def recv(self):
         #TODO: Add Anti-Replay Mechanism and HMAC
-        # Decode the data's length from an unsigned two byte int ('H')
-        pkt_len_packed = self.conn.recv(struct.calcsize('H'))
-        unpacked_contents = struct.unpack('H', pkt_len_packed)
-        pkt_len = unpacked_contents[0]
 
+        # Decode the data's length from an unsigned two byte int ('H')        
+        pkt_len_packed = self.conn.recv(struct.calcsize('H'))
+        pkt_len = struct.unpack('H', pkt_len_packed)[0]
+        
+        # Recieve the encrypted data
         encrypted_data = self.conn.recv(pkt_len)
-        if self.cipher:
-            data = self.cipher.decrypt(encrypted_data)
-            if self.verbose:
-                print("Receiving packet of length {}".format(pkt_len))
-                print("Encrypted data: {}".format(repr(encrypted_data)))
-                print("Original data: {}".format(data))
+
+        if self.decryptCipher:
+            data = self.decryptCipher.decrypt(encrypted_data)
+            
+            self.__print_verbose("Receiving packet of length {}".format(pkt_len))
+            self.__print_verbose("Encrypted data: {}".format(repr(encrypted_data)))
+            self.__print_verbose("Original data: {}".format(data))    
         else:
             data = encrypted_data
 
         return data
-    
-    def encrypt(self, data):
-        print("Encrypting...") #Remove when function implemented
-        ## TODO - Implement block cipher
-        # cipher = AES.new(their_public_key, MODE, iv)
-    
-    def decrypt(self, data):
-        print("Decrypting...") #Remove when function implemented
-        ## TODO - Implement block cipher
-        # cipher = AES.new(our_private_key, MODE, iv)
-        
 
     def close(self):
         self.conn.close()
