c5a880e6e5ea49c5f1891d6f6625c945ce80f677
Andrew <andrew.pope456@gmail.com>
2016-05-03 04:24:59 -0400

    Replay Attacks: - Rather than using session nonces, a counter variables will now be used to prevent replay attacks. In the old implementation (if implemented correctly - which it wasn't) a huge list would have been needed to maintain knowledge over the used session nonces.

--- lib/comms.py
+++ lib/comms.py
@@ -12,9 +12,9 @@ class StealthConn(object):
     self.conn               = conn
     self.client             = client
     self.server             = server
-    self.verbose            = verbose    
+    self.verbose            = verbose
+    self.session_counter    = 0    
     # Note that all variables below are stored in byte format
-    self.session_nonce_hash = None
     self.my_private_key     = None
     self.their_public_key   = None
     self.shared_hash        = None
@@ -44,39 +44,7 @@ class StealthConn(object):
     
     return self.conn.recv(pkt_len), pkt_len
 
-  def __sync_session_nonces(self, shared_hash):
-    """
-    Used to generate and synchronise nonces between
-    client and server. This is used to prevent relplay
-    attacks.
-    """
-    if self.server:
-      # Generate the pseudorandom server nonce & send to client
-      snonce = bytes(str(random.randint(1, 2**4096)), "ascii")
-      self.__packet_send(snonce)
-      
-      # Recieve the session_nonce_hash and cnonce from the client, recalculate snh and compare
-      snh, pkt_len    = self.__packet_recv()
-      cnonce, pkt_len = self.__packet_recv()
-      
-      calc_nonce              = snonce + cnonce + shared_hash
-      self.session_nonce_hash = bytes(SHA256.new(bytes(calc_nonce)).hexdigest(), "ascii")
-      
-      if self.session_nonce_hash != snh:
-        self.close() # Dubious activity detected - shutdown the connection
-       
-    if self.client:
-      # Generate the pseudorandom client nonce and recieve the server nonce
-      cnonce = bytes(str(random.randint(1, 2**4096)), "ascii")
-      snonce, pkt_len = self.__packet_recv()
 
-      # Calculate the session nonces with shared secret hash
-      calculated_nonce = snonce + cnonce + shared_hash
-      self.session_nonce_hash = bytes(SHA256.new(bytes(calculated_nonce)).hexdigest(), "ascii")
-      
-      # Encode the string to bytes for transmission and send
-      self.__packet_send(self.session_nonce_hash)
-      self.__packet_send(cnonce)
     
   def initiate_session(self):
     """
@@ -100,16 +68,13 @@ class StealthConn(object):
       # allowed only in AES encryption cipher)
       self.their_public_key = bytes(str(self.their_public_key), "ascii").ljust(32)[:32]
       self.my_private_key   = bytes(str(self.my_private_key), "ascii").ljust(32)[:32]
-      
-      # Perform synch of session nonces as Anti-Replay Mechanism
-      self.__sync_session_nonces(self.shared_hash)
 	
   def send(self, data):
     """
     Encrypt and send data over the network.
     """
     # Initialise the encrypt Cipher (a new IV will be generated for each encyrption)
-    iv = Random.OSRNG.posix.new().read(AES.block_size)
+    iv = Random.get_random_bytes(AES.block_size)
     cipher = AES.new(self.their_public_key, AES.MODE_OFB, iv)
     
     # Record the original len of the data, and pad message out
@@ -127,13 +92,17 @@ class StealthConn(object):
     hmac.update(encrypted_data)
     
     # Encode information so its in byte format
-    hmac = bytes(str(hmac.hexdigest()), "ascii")
+    hmac    = bytes(str(hmac.hexdigest()), "ascii")
+    counter = bytes(str(self.session_counter), "ascii") 
     
     # Send the encrypted data with relevant information
-    self.__packet_send(self.session_nonce_hash) # Send the session nonce (for Anti-Replay attacks)
+    self.__packet_send(counter)                 # Send the session counter (for Anti-Replay attacks)
     self.__packet_send(iv)                      # Send the iv (to create the decrypt cipher)
     self.__packet_send(encrypted_data)          # Send the encrypted data
     self.__packet_send(hmac)                    # Send the hmac
+    
+    # Increment the session counter, so that the next message will have a new 'unique' identifier
+    self.session_counter += 1
 	  
 	
   def recv(self):
@@ -141,7 +110,7 @@ class StealthConn(object):
     Recieve and decrypt data from the network.
     """
     # Recieve the encrypted data with relevant information
-    snh, pkt_len                = self.__packet_recv() # The session nonce hash
+    counter, pkt_len            = self.__packet_recv() # The session nonce hash
     iv, pkt_len                 = self.__packet_recv() # The iv used in encryption
     encrypted_data, encrypt_len = self.__packet_recv() # The encrypted data
     hmac, pkt_len               = self.__packet_recv() # The hashed Message Authentication Code
@@ -154,19 +123,26 @@ class StealthConn(object):
     calc_hmac.update(encrypted_data)
     calc_hmac = bytes(str(calc_hmac.hexdigest()), "ascii")
     
+    # Convert session counter to byte format
+    this_counter = bytes(str(self.session_counter), "ascii")
     data = None
     
     # Perform Anti-Replay check
-    if snh == self.session_nonce_hash:
+    if counter == this_counter:
       # Autenticate with HMAC
       if calc_hmac == hmac:      
         # Decrypt data
         data = cipher.decrypt(encrypted_data)
         data = ANSI_X923_unpad(data, cipher.block_size)
+        
+        # Increment session counter, to keep lock-step
+        # with the other bot
+        self.session_counter += 1
 
         self.__print_verbose("Receiving packet of length {}".format(encrypt_len))
         self.__print_verbose("Encrypted data: {}".format(repr(encrypted_data)))
-        self.__print_verbose("Original data: {}".format(data))    
+        self.__print_verbose("Original data: {}".format(data)) 
+          
       else:
         self.__print_verbose("Autentication Failed!")
         self.close()
