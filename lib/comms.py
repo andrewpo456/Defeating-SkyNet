1fe3db95a1f9bdcc63f8a18b2cdab2228a50cd2e
Andrew <andrew.pope456@gmail.com>
2016-05-04 00:59:33 -0400

    Comms: - Updating the recv() routine to handle this 'one packet' approach to relaying messages between bots.

--- lib/comms.py
+++ lib/comms.py
@@ -13,11 +13,17 @@ class StealthConn(object):
     self.client             = client
     self.server             = server
     self.verbose            = verbose
-    self.session_counter    = 0    
+    self.session_counter    = 0
+        
     # Note that all variables below are stored in byte format
     self.my_private_key     = None
     self.their_public_key   = None
     self.shared_hash        = None
+    
+    # These are class constants must not be changed
+    self.HMAC_LEN          = 64 # We are using SHA256, thefore each HMAC is 64 bytes long
+    self.COUNTER_LEN       = 7  # We declare that the counter is sent as a 7 byte number max
+                                # counter value is therefore '1000000'
     
     self.initiate_session()
 
@@ -60,94 +66,97 @@ class StealthConn(object):
       pubKey, key_len = self.__packet_recv()
       self.their_public_key = int(pubKey)
 
-      # Obtain the shared secret
+      # Obtain a shared secret
       self.shared_hash = calculate_dh_secret(self.their_public_key, self.my_private_key)
       self.shared_hash = bytes(self.shared_hash, "ascii")        	
       
-      # Convert the keys to byte format and truncate to a size of 32 (16 ,24 and 32 bit keys
-      # allowed only in AES encryption cipher)
-      self.their_public_key = bytes(str(self.their_public_key), "ascii").ljust(32)[:32]
-      self.my_private_key   = bytes(str(self.my_private_key), "ascii").ljust(32)[:32]
+      # Convert the keys to byte format and truncate to 16 bytes (required key size for 128-bit AES)
+      self.their_public_key = bytes(str(self.their_public_key), "ascii")[:16]
+      self.my_private_key   = bytes(str(self.my_private_key), "ascii")[:16]
 	
   def send(self, data):
     """
     Encrypt and send data over the network.
     """
     self.__print_verbose("Original data: {}".format(data))
-    
     # Initialise the encrypt Cipher (a new IV will be generated for each encyrption)
-    iv = Random.get_random_bytes(AES.block_size)
-    cipher = AES.new(self.their_public_key, AES.MODE_OFB, iv)
+    iv      = Random.get_random_bytes(AES.block_size)
+    cipher  = AES.new(self.their_public_key, AES.MODE_OFB, iv)
     
     # Create the HMAC = hash( shared_secret + session_counter + plaintext)
-    # and convert to bytes
     hmac = HMAC.new(self.shared_hash, digestmod=SHA256)
     hmac.update(bytes(str(self.session_counter), "ascii"))
     hmac.update(data)
-    hmac = bytes(str(hmac), "ascii")
     
-    # Construct the message = session_counter + data + HMAC 
-    ctr_str = bytes(str(self.session_counter), "ascii")
-    ctr_str = ANSI_X923_pad(ctr_str, 7) # The data is padded to 7 bytes as this is what reciever expects
-    message = ctr_str + data + hmac
+    # Construct the data to encrypt = session_counter +  HMAC + data 
+    hmac            = bytes(str(hmac.hexdigest()), "ascii")
+    ctr_str         = bytes(str(self.session_counter), "ascii")
+    ctr_str         = ANSI_X923_pad(ctr_str, self.COUNTER_LEN)
+    data_to_encrypt = ctr_str + hmac + data 
     
     # Encrypt the message
-    message = ANSI_X923_pad(message, cipher.block_size) # TODO: Determine if need to pad in OFB is bug or not
-    encrypted_data = cipher.encrypt(message)
-    self.__print_verbose("Sending packet of length {}".format(len(encrypted_data)))
-    self.__print_verbose("Encrypted data: {}".format(repr(encrypted_data)))
+    data_to_encrypt = ANSI_X923_pad(data_to_encrypt, cipher.block_size)
+    encrypted_data  = cipher.encrypt(data_to_encrypt)
+    self.__print_verbose("Encrypted Data: {}".format(repr(encrypted_data)))
     
-    # Append the iv
-    message_to_send = iv + encrypted_data
+    # Append the iv to create the packet
+    packet = iv + encrypted_data
     
-    # Send the encrypted data with relevant information
-    self.__packet_send(message_to_send) # Send the encrypted data
+    # Send the packet
+    self.__print_verbose("Sending packet of length {}".format(len(packet)))
+    self.__packet_send(packet)
     
-    # Increment the session counter, so that the next message will have a new 'unique' identifier
+    # Increment the session counter, so that the next message will have a new 'unique' freshness identifier
     self.session_counter += 1
 	
   def recv(self):
     """
     Recieve and decrypt data from the network.
     """
-    # Recieve the message
-    message, encrypt_len = self.__packet_recv() # The encrypted data
+    # Recieve the paket
+    packet, pkt_len = self.__packet_recv() # The encrypted data
+    self.__print_verbose("Receiving packet of length {}".format(pkt_len))
     
-    iv = message[:16]
-    encrypted_data = message[16:]
+    # Split the packet into iv and encrypted data
+    iv = packet[:AES.block_size]
+    encrypted_data = packet[AES.block_size:]
+    self.__print_verbose("Encrypted Data: {}".format(repr(encrypted_data)))
     
-    # Initialize decrypt cipher
-    cipher = AES.new(self.my_private_key, AES.MODE_OFB, iv)
+    # Initialize decrypt cipher and decrypt data
+    cipher  = AES.new(self.my_private_key, AES.MODE_OFB, iv)
+    message = cipher.decrypt(encrypted_data)
     
-    # Calculate hmac
+    # Split the data into counter, HMAC and plaintext
+    message      = ANSI_X923_unpad(message, cipher.block_size) 
+    recv_counter = ANSI_X923_unpad(message[:self.COUNTER_LEN], self.COUNTER_LEN)      
+    recv_hmac    = message[self.COUNTER_LEN:(self.COUNTER_LEN + self.HMAC_LEN)]                       
+    plaintext    = message[(self.COUNTER_LEN + self.HMAC_LEN):]
+    
+    # Calculate our own hmac to verify integrity
     calc_hmac = HMAC.new(self.shared_hash, digestmod=SHA256)
-    calc_hmac.update(encrypted_data)
-    calc_hmac = bytes(str(calc_hmac.hexdigest()), "ascii")
+    calc_hmac.update(recv_counter)
+    calc_hmac.update(plaintext)
     
-    # Convert session counter to byte format
+    # Convert to byte format for comparing data
+    calc_hmac = bytes(str(calc_hmac.hexdigest()), "ascii")
     this_counter = bytes(str(self.session_counter), "ascii")
-    data = None
     
     # Perform Anti-Replay check
-    if counter == this_counter:
+    if recv_counter == this_counter:
       # Autenticate with HMAC
-      if calc_hmac == hmac:      
-        # Decrypt data
-        data = cipher.decrypt(encrypted_data)
-        data = ANSI_X923_unpad(data, cipher.block_size)
-        
+      if calc_hmac == recv_hmac:              
+        data = plaintext
+        self.__print_verbose("Original data: {}".format(data))         
+
         # Increment session counter, to keep lock-step
         # with the other bot
-        self.session_counter += 1
-
-        self.__print_verbose("Receiving packet of length {}".format(encrypt_len))
-        self.__print_verbose("Encrypted data: {}".format(repr(encrypted_data)))
-        self.__print_verbose("Original data: {}".format(data)) 
-          
+        self.session_counter += 1          
       else:
-        self.__print_verbose("Autentication Failed!")
+        data = None
+        self.__print_verbose("Integrity Check Failed!")
         self.close()
     else:
+      data = None
       self.__print_verbose("Replay Attack detected!")
       self.close()
 
